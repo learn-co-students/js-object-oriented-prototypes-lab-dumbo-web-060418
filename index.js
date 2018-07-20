fe585a3926a1a789b6614e568b411054912ec997
Eric Razor, Jr <eric.razorjr@gmail.com>
2018-07-20 12:39:51 -0400

    Done.

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ function BoardMember(name, homeState, training){
   this.training = training
 
 
-  // this.veto = function(){
+  // veto() {
   //   return 'No, I must disagree'
   // }
-  this.approve = function(){
-    return 'You can do that!'
-  }
-  this.doCharity = function(){
-    return "I like to help people."
-  }
-
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+  // this.approve = function(){
+  //   return 'You can do that!'
+  // }
+  // this.doCharity = function(){
+  //   return "I like to help people."
+  // }
+  //
+  // this.releasePressStatement = function(){
+  //   return "You will see great things from Scuber."
+  // }
+  //
+  // this.sayHi = function(){
+  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  // }
 }
 
 BoardMember.prototype.veto = function() {
@@ -36,11 +36,11 @@ BoardMember.prototype.doCharity = function() {
 }
 
 BoardMember.prototype.releasePressStatement = function() {
-  return 'You will see great things from Scuber'
+  return 'You will see great things from Scuber.'
 }
 
 BoardMember.prototype.sayHi = function() {
-  return "Hi, my name is `${this.name}`. I am from `${this.homeState}`, and I was trained in `${this.training}`"
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
 
