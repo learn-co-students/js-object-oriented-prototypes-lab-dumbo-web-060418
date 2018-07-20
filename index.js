d6f1f7b3392f0f8e7298ddb5217ea7fed56ea073
robertexe <robtabares92@gmail.com>
2018-07-20 08:43:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
+// Add following methods to the prototype of the constructor:
 //
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
+// veto — returns No, I must disagree
 //
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
+// approve — returns You can do that!
 //
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+// doCharity — returns I like to help people.
+//
+// releasePressStatement — returns You will see great things from Scuber.
+//
+// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+//
+
+
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+  BoardMember.prototype.veto = () => 'No, I must disagree'
+  BoardMember.prototype.approve = () => 'You can do that!'
+  BoardMember.prototype.doCharity = () => "I like to help people."
+  BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber."
+
+  BoardMember.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
